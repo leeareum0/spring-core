3838d244e93a719f49e5e72ed5373c3d203a589e
이아름 <areum6321@gmail.com>
2024-06-19 00:43:22 +0900

    싱글톤 컨테이너 - 스프링(싱글톤) 컨테이너 사용 테스트 코드

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,7 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,5 +48,26 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
 
         singletonService1.logic();
+    }
+
+    //스프링 컨테이너 사용
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회 : 호출할 때마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회 : 호출할 때마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //3. 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
