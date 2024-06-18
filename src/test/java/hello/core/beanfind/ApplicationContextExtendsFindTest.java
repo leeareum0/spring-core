43a8f3f5d6aff8e7843a37fa3e1d57be7068aa42
이아름 <areum6321@gmail.com>
2024-06-18 19:23:59 +0900

    스프링 빈 조회 - 상속 관계, 부모 타입으로 조회 시 빈 이름 지정

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -24,6 +24,13 @@ public class ApplicationContextExtendsFindTest {
         assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
     }
 
+    @Test
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByParentTypeBeanName() {
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
 
     @Configuration
     static class TestConfig {
